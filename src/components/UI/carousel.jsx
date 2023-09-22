ff14c805301a35c2657e2d16ddfbcacdc3c7240c
Patrik Flegl <fleglpa20@sps-prosek.cz>
2023-09-22 12:05:48 +0200

    rating commit

--- src/components/UI/carousel.jsx
+++ src/components/UI/carousel.jsx
@@ -6,10 +6,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-
 // import required modules
 import { Navigation } from 'swiper/modules';
-
 export default function App() {
     let jednicka;
     jednicka = "src/png/room.png"
