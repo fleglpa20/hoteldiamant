39a80ceb06096be23d84ee036855c75e19bccf7c
Patrik Flegl <fleglpa20@sps-prosek.cz>
2023-09-22 15:09:15 +0200

    step

--- src/components/UI/carousel.jsx
+++ src/components/UI/carousel.jsx
@@ -8,18 +8,20 @@ import 'swiper/css/navigation';
 
 // import required modules
 import { Navigation } from 'swiper/modules';
+
 export default function App() {
-    let jednicka;
-    jednicka = "src/png/room.png"
     return (
         <>
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide><img src={jednicka}/></SwiperSlide>
-                <SwiperSlide><img src={jednicka}/></SwiperSlide>
-                <SwiperSlide><img src={jednicka}/></SwiperSlide>
-                <SwiperSlide><img src={jednicka}/></SwiperSlide>
-                <SwiperSlide><img src={jednicka}/></SwiperSlide>
-                <SwiperSlide><img src={jednicka}/></SwiperSlide>
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
             </Swiper>
         </>
     );
